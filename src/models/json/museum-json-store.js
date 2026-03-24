7a5fdb79b173a6d553696d73f78a6abb39242735
Marcia Satie <111194629+MarciaSatie@users.noreply.github.com>
2026-03-24 20:16:42 +0000

    fixing delete museum

--- src/models/json/museum-json-store.js
+++ src/models/json/museum-json-store.js
@@ -33,8 +33,12 @@ export const museumJsonStore = {
   async deleteMuseumById(id) {
     await db.read();
     const index = db.data.museums.findIndex((m) => m._id === id);
-    await exhibitionJsonStore.deleteExhibitionsByMuseumId(museum._id);
-    db.data.museums.splice(index, 1);
+    const museum = db.data.museums[index];
+    if (museum) {
+      await exhibitionJsonStore.deleteExhibitionsByMuseumId(museum._id);
+      db.data.museums.splice(index, 1);
+      await db.write();
+    }
     await db.write();
   },
 
